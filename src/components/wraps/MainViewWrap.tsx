cf3b76445ce9884b4ca695b34581dcd9556bde94
marcomariscal <marco.a.mariscal@gmail.com>
2021-09-15 17:54:11 -0400

    adding padding to bottom

--- src/components/wraps/MainViewWrap.tsx
+++ src/components/wraps/MainViewWrap.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const MainViewWrap = ({ children }: any) => <div className="flex justify-center sm:pt-8 md:pt-20">{children}</div>;
+const MainViewWrap = ({ children }: any) => (
+  <div className="flex justify-center sm:pt-8 md:pt-20 md:pb-20">{children}</div>
+);
 
 export default MainViewWrap;
