a1a0801b9c437d3f69b0f6a5a505579a2cc771ba
marcomariscal <marco.a.mariscal@gmail.com>
2021-09-23 12:36:34 -0400

    adding args to events

--- src/state/actions/contracts.ts
+++ src/state/actions/contracts.ts
@@ -9,12 +9,13 @@ export function getEvents(contractMap: any, contractAddr: any, filter = '*') {
       try {
         dispatch(setEventsLoading(true));
         const events = await contract.queryFilter(filter, null, null);
+        console.log(events);
         const updatedEvents = events.map((e: any, i: number) => ({
           id: i,
           event: e.event,
           blockNumber: e.blockNumber,
+          args: e.args.join(', '),
         }));
-        console.log(updatedEvents);
 
         const eventsMap = { [contractAddr]: updatedEvents };
 
