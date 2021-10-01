371e18ac80a09160275623abef5de31884818666
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-01 11:52:14 -0400

    adding filter by owner

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { FiSearch } from 'react-icons/fi';
 
-const SearchInput = ({ name, value, action }: { name: string; value: string; action: any }) => (
+const SearchInput = ({
+  name,
+  value,
+  action,
+  placeHolder,
+}: {
+  name: string;
+  value: string;
+  action: any;
+  placeHolder: string;
+}) => (
   <div className="relative rounded-md shadow-sm bg-green-300 h-full w-full ">
     <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
       <span className="text-green-500 sm:text-md">
@@ -12,7 +22,7 @@ const SearchInput = ({ name, value, action }: { name: string; value: string; act
       type="text"
       name={name}
       className="p-3 block w-full pl-10 pr-12 sm:text-sm rounded-md h-full bg-green-50  focus:ring-0 ring-0"
-      placeholder="Vault Id"
+      placeholder={placeHolder}
       value={value}
       onChange={action}
     />
