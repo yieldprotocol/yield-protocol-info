d5ca4c6711d82d850dbf4b9b40335874d5cd521a
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-04 17:18:46 +0100

    updating target link

--- src/components/AddressDisplay.tsx
+++ src/components/AddressDisplay.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useAppSelector } from '../state/hooks/general';
+import { NETWORK_LABEL as chainData } from '../config/networks';
 
 const AddressDisplay = ({ addr }: any) => {
-  const chainId = useAppSelector((st: any) => st.chain?.chainId);
-  const linkDomain = chainId === 42 ? `https://kovan.etherscan.io/` : `https://etherscan.io/address/`
-  const linkPath = `${linkDomain}/address/${addr}/`
+  const chainId = useAppSelector((st: any) => st.chain.chainId);
   return (
-    <a href={linkPath} target="_blank" rel="noreferrer">
-    <code className="text-smt hover:underline">{addr}</code>
-  </a>
-  )
+    <a
+      href={`https://${chainId === '1' ? '' : `${chainData[chainId]}.`}etherscan.io/address/${addr}`}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <code className="text-smt hover:underline">{addr}</code>
+    </a>
+  );
 };
 
 export default AddressDisplay;
