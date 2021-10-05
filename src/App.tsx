c14216ebbee58adb54add14beeafc626d927cf3e
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-05 11:02:07 +0100

    only rendering the vaults on initial load

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Routes from './components/Routes';
 import Navigation from './components/Navigation';
 import { useChain } from './state/hooks/useChain';
+import { useVaults } from './state/hooks/useVaults';
 
 function App() {
   useChain();
+  useVaults();
 
   return (
     <div className="">
