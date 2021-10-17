78f10d665fb39371e966b7c5208cff576e36a898
Richie Humphrey <eihcir0@gmail.com>
2021-10-16 21:27:39 -0700

    fix(batchdecoder): updates logic in hook and display

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -4,18 +4,20 @@ const TextInput = ({
   name,
   value,
   action,
+  onChange,
   placeHolder,
 }: {
   name: string;
   value: string;
   action: any;
+  onChange: any;
   placeHolder: string;
 }) => (
   <div className="relative rounded-md shadow-sm bg-green-300 h-full w-full ">
     <input
       type="text"
       placeholder={placeHolder}
-      onChange={(e) => action(e.target.value)}
+      onChange={(e) => onChange(e.target.value)}
       className="p-3 block w-full pl-10 pr-12 sm:text-sm rounded-md h-full bg-green-50 focus:ring-0 ring-0"
       value={value}
       onKeyPress={(e) => {
