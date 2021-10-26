f3e58eb917a96aeb4c6c6641d1c0a057f3bf275f
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-26 16:20:28 -0400

    add asset pair data

---
+++ src/components/AssetPairTable.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { v4 as uuid } from 'uuid';
+import { markMap } from '../config/marks';
+
+const AssetPairTable = ({ data, assets }: any) =>
+  data ? (
+    <div className="rounded-md shadow-sm px-8 py-4 bg-green-50">
+      <table className="table min-w-full divide-y divide-gray-200">
+        <thead>
+          <tr>
+            {['', 'Min Collat Ratio', 'Min Debt', 'Max Debt'].map((x) => (
+              <th
+                key={x}
+                scope="col"
+                className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider justify-items-start text-center"
+              >
+                {x}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-green divide-y divide-gray-200">
+          {data.map((x: any) => {
+            const baseAsset = assets[x.baseAssetId];
+            const ilkAsset = assets[x.ilkAssetId];
+            const baseAssetLogo = markMap?.get(baseAsset?.symbol!);
+            const ilkAssetLogo = markMap?.get(ilkAsset?.symbol!);
+            return (
+              <tr key={uuid()} className="items-center  dark:border-green-700 group">
+                <td className="px-2 py-2 text-center items-center">
+                  <div className="flex relative">
+                    <div className="h-7 w-7">
+                      <div className="z-0">{baseAssetLogo}</div>
+                    </div>
+                    <div className="z-10 -ml-2">
+                      <div className="h-7 w-7">{ilkAssetLogo}</div>
+                    </div>
+                  </div>
+                </td>
+                <td className="px-6 py-3 text-center items-center">
+                  <span className="text-sm font-medium text-gray-900 dark:text-white truncate">
+                    <span>{x.minCollatRatioPct}</span>
+                  </span>
+                </td>
+                <td className="px-6 py-3 text-center items-center">
+                  <span className="text-sm font-medium text-gray-900 dark:text-white truncate">
+                    <span>{x.minDebt}</span>
+                  </span>
+                </td>
+                <td className="px-6 py-3 text-center items-center">
+                  <span className="text-sm font-medium text-gray-900 dark:text-white truncate">
+                    <span>{x.maxDebt}</span>
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  ) : null;
+
+export default AssetPairTable;
--- src/components/views/Asset.tsx
+++ src/components/views/Asset.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useAppSelector } from '../../state/hooks/general';
+import { useAppDispatch, useAppSelector } from '../../state/hooks/general';
 import SingleItemViewGrid from '../wraps/SingleItemViewGrid';
 import MainViewWrap from '../wraps/MainViewWrap';
+import AssetPairTable from '../AssetPairTable';
 import { markMap } from '../../config/marks';
+import { getAssetPairData } from '../../state/actions/chain';
 
 const Asset = () => {
   const { id } = useParams<{ id: string }>();
+  const dispatch = useAppDispatch();
   const assets = useAppSelector((st) => st.chain.assets);
+  const assetPairData = useAppSelector((st) => st.chain.assetPairData);
+  const contractMap = useAppSelector((st) => st.contracts.contractMap);
   const asset = assets[id];
+  const thisAssetPairData = assetPairData[asset.id];
   const logo = markMap?.get(asset?.symbol);
+
+  useEffect(() => {
+    dispatch(getAssetPairData(asset, assets, contractMap));
+  }, [dispatch, asset, assets, contractMap]);
 
   return asset ? (
     <MainViewWrap>
-      <div className="rounded-md p-8 align-middle justify-items-start shadow-sm bg-green-50">
-        <div className="text-lg pb-4 flex gap-x-2">
-          {logo && <div className="h-6 w-6">{logo}</div>}
-          <strong>{asset.symbol}</strong>
+      <div className="flex gap-10">
+        <div className="rounded-md p-8 align-middle justify-items-start shadow-sm bg-green-50">
+          <div className="text-lg pb-4 flex gap-x-2">
+            {logo && <div className="h-6 w-6">{logo}</div>}
+            <strong>{asset.symbol}</strong>
+          </div>
+          <div>
+            <SingleItemViewGrid item={asset} />
+          </div>
         </div>
-        <SingleItemViewGrid item={asset} />
+        {thisAssetPairData?.length && assets && (
+          <div className="rounded-md p-8 align-middle justify-items-start shadow-sm">
+            <AssetPairTable data={thisAssetPairData} assets={assets} />
+          </div>
+        )}
       </div>
     </MainViewWrap>
   ) : null;
