db0b8ea35106a7eb5d39426d4289209ecc5d4977
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-26 08:12:11 -0400

    fix network labeling for api calls

--- src/components/views/BatchDecoder.tsx
+++ src/components/views/BatchDecoder.tsx
@@ -6,55 +6,59 @@ import { useBatchDecoder } from '../../hooks/useBatchDecoder';
 import Button from '../Button';
 import AddressDisplay from '../AddressDisplay';
 
-
 const CallDisplay = ({ call }: any): any => (
-    <table>
-      <tbody>
-        <tr style={{ paddingTop: '1rem' }} className="font-bold">
-          <td>{call.method}</td>
-        </tr>
-        {call.arguments?.map((ogArgs: any, idx: number) => {
-          const args: any = Array.isArray(ogArgs) ? ogArgs : [ogArgs];
-          return (
-            <>
-              <tr style={{ paddingLeft: '2rem' }} className="no-wrap">
-                <td style={{ paddingLeft: '2rem', paddingRight: '2rem' }} className="italic">
-                  {call.argProps[idx].name}
-                </td>
-                <td>
-                  {/** If there's only one argument (and it's not an object), display it right next to the arg name */}
-                  {args.length === 1 &&
-                    (!(args[0] instanceof Object) && (call.argProps[idx].type === 'address' ? <AddressDisplay addr={args[0]}/> : (
-                      <span>
-                        <code>{args[0]}</code>
-                      </span>
-                    )))}
-                </td>
-              </tr>
-              {(args.length > 1 || (args.length === 1 && args[0] instanceof Object)) &&
-                args.map((v: any) => (
-                  <tr key={uuid()}>
-                    {v instanceof Object ? (
-                      <>
-                        <td style={{ paddingLeft: '2rem', paddingRight: '2rem' }} />
-                        <td>
-                          <CallDisplay call={v} />
-                        </td>
-                      </>
-                    ) : (
-                      <>
-                        <td style={{ paddingLeft: '2rem', paddingRight: '2rem' }} />
-                        <td><code>{JSON.stringify(v)}</code></td>
-                      </>
-                    )}
-                  </tr>
-                ))}
-            </>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+  <table>
+    <tbody>
+      <tr style={{ paddingTop: '1rem' }} className="font-bold">
+        <td>{call.method}</td>
+      </tr>
+      {call.arguments?.map((ogArgs: any, idx: number) => {
+        const args: any = Array.isArray(ogArgs) ? ogArgs : [ogArgs];
+        return (
+          <>
+            <tr style={{ paddingLeft: '2rem' }} className="no-wrap">
+              <td style={{ paddingLeft: '2rem', paddingRight: '2rem' }} className="italic">
+                {call.argProps[idx].name}
+              </td>
+              <td>
+                {/** If there's only one argument (and it's not an object), display it right next to the arg name */}
+                {args.length === 1 &&
+                  !(args[0] instanceof Object) &&
+                  (call.argProps[idx].type === 'address' ? (
+                    <AddressDisplay addr={args[0]} />
+                  ) : (
+                    <span>
+                      <code>{args[0]}</code>
+                    </span>
+                  ))}
+              </td>
+            </tr>
+            {(args.length > 1 || (args.length === 1 && args[0] instanceof Object)) &&
+              args.map((v: any) => (
+                <tr key={uuid()}>
+                  {v instanceof Object ? (
+                    <>
+                      <td style={{ paddingLeft: '2rem', paddingRight: '2rem' }} />
+                      <td>
+                        <CallDisplay call={v} />
+                      </td>
+                    </>
+                  ) : (
+                    <>
+                      <td style={{ paddingLeft: '2rem', paddingRight: '2rem' }} />
+                      <td>
+                        <code>{JSON.stringify(v)}</code>
+                      </td>
+                    </>
+                  )}
+                </tr>
+              ))}
+          </>
+        );
+      })}
+    </tbody>
+  </table>
+);
 
 const BatchDecoder = () => {
   const [txHash, setTxHash] = useState('');
