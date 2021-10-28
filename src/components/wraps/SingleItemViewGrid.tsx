80e597dbf919c98c42b5de46ece8148ccbe0d54f
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-28 18:01:24 -0400

    update grid to handle address display

--- src/components/wraps/SingleItemViewGrid.tsx
+++ src/components/wraps/SingleItemViewGrid.tsx
@@ -1,15 +1,29 @@
+import { ethers } from 'ethers';
 import React from 'react';
+import AddressDisplay from '../AddressDisplay';
 
 const SingleItemViewGrid = ({ item }: any) => (
   <div className="grid grid-rows-auto gap-y-8">
-    {Object.keys(item).map((key: any) => (
-      <div className="text-sm flex gap-x-10" key={key}>
-        <div className="w-20">
-          <i>{key}</i>
+    {Object.keys(item).map((key: any) => {
+      const val = item[key];
+      return ethers.utils.isAddress(val) ? (
+        <div className="text-sm flex gap-x-10" key={key}>
+          <div className="w-20">
+            <i>{key}</i>
+          </div>
+          <div className="justify-self-start">
+            <AddressDisplay addr={val} />
+          </div>
         </div>
-        <div className="justify-self-start">{item[key]}</div>
-      </div>
-    ))}
+      ) : (
+        <div className="text-sm flex gap-x-10" key={key}>
+          <div className="w-20">
+            <i>{key}</i>
+          </div>
+          <div className="justify-self-start">{item[key]}</div>
+        </div>
+      );
+    })}
   </div>
 );
 
