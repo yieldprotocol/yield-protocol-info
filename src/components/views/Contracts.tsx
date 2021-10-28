8cef4fcd35198c4ca50afc4a3c62f331f0167555
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-28 17:47:59 -0400

    sort contracts by name

--- src/components/views/Contracts.tsx
+++ src/components/views/Contracts.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../../state/hooks/general';
 import ContractItem from '../ContractItem';
 import MainViewWrap from '../wraps/MainViewWrap';
 
 const Contracts = () => {
   const contractMap = useAppSelector((st) => st.contracts.contractMap);
+  const [contractsList, setContractsList] = useState<any[]>([]);
+
+  useEffect(() => {
+    Object.values(contractMap).length > 0 &&
+      setContractsList([...Object.values(contractMap)].sort((s1: any, s2: any) => (s1?.name! < s2?.name! ? -1 : 1)));
+  }, [contractMap]);
 
   if (!Object.values(contractMap).length) return <MainViewWrap>No Contracts</MainViewWrap>;
 
   return (
     <MainViewWrap>
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {[...Object.values(contractMap)].map((item: any) => (
+        {contractsList.map((item: any) => (
           <ContractItem item={item} key={item.contract.address} />
         ))}
       </div>
