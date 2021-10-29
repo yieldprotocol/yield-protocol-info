57d35e6cc0c9799d147f4cb0cdc618ea2be7183c
marcomariscal <marco.a.mariscal@gmail.com>
2021-10-29 17:23:28 -0400

    update to handle not an address

--- src/components/AddressDisplay.tsx
+++ src/components/AddressDisplay.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ethers } from 'ethers';
 import { useAppSelector } from '../state/hooks/general';
 import { NETWORK_LABEL as chainData } from '../config/networks';
 
 const AddressDisplay = ({ addr, tx }: any) => {
   const chainId = useAppSelector((st: any) => st.chain.chainId);
-  return (
+
+  if (!addr) return null;
+
+  return ethers.utils.isAddress(addr) ? (
     <a
       href={`https://${chainId === '1' ? '' : `${chainData[chainId]}.`}etherscan.io/${
         tx ? 'tx' : 'address'
@@ -14,6 +18,8 @@ const AddressDisplay = ({ addr, tx }: any) => {
     >
       <code className="text-smt hover:underline">{addr}</code>
     </a>
+  ) : (
+    <code className="text-smt">{addr}</code>
   );
 };
 
