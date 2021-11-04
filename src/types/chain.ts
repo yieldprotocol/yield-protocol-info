e0f3c967e81d8dfb3c4ab8734b7e8a80245eac4c
marcomariscal <marco.a.mariscal@gmail.com>
2021-11-04 16:20:37 -0400

    update types

--- src/types/chain.ts
+++ src/types/chain.ts
@@ -1,103 +1,52 @@
-import { ethers, BigNumber, BigNumberish } from 'ethers';
-// import { FYToken, Pool, Strategy } from '../contracts';
+import { BigNumber } from 'ethers';
 
-export interface ISignable {
+export interface ISeries {
+  id: string;
+  baseId: string;
+  maturity: number;
   name: string;
+  symbol: string;
   version: string;
   address: string;
-  symbol: string;
-}
-
-export interface ISeriesRoot extends ISignable {
-  id: string;
-  displayName: string;
-  displayNameMobile: string;
-  maturity: number;
-  fullDate: Date;
-  // fyTokenContract: FYToken;
   fyTokenAddress: string;
-  // poolContract: Pool;
-  poolAddress: string;
-  poolName: string;
-  poolVersion: string; // for signing
-  poolSymbol: string; // for signing
-
   decimals: number;
+  poolAddress: string;
+  poolVersion: string;
+  poolName: string;
+  poolSymbol: string; // for signing
+  fullDate: Date;
+  displayName: string;
+  season: string;
 
-  baseId: string;
-
-  color: string;
-  textColor: string;
   startColor: string;
   endColor: string;
-
-  oppositeColor: string;
+  color: string;
+  textColor: string;
   oppStartColor: string;
   oppEndColor: string;
-
-  seriesMark: React.ElementType;
-
-  // baked in token fns
-  getTimeTillMaturity: () => string;
-  isMature: () => boolean;
-  getBaseAddress: () => string; // antipattern, but required here because app simulatneoulsy gets assets and series
+  oppTextColor: string;
 }
 
-export interface IAssetRoot extends ISignable {
-  // fixed/static:
+export interface IAsset {
   id: string;
+  address: string;
+  name: string;
+  symbol: string;
   decimals: number;
-  color: string;
-  image: React.FC;
-  displayName: string;
-  displayNameMobile: string;
+  version: string;
   joinAddress: string;
   digitFormat: number;
-
-  // baseContract: ERC20Permit;
-
-  // baked in token fns
-  getBalance: (account: string) => Promise<BigNumber>;
-  getAllowance: (account: string, spender: string) => Promise<BigNumber>;
-  mintTest: () => Promise<VoidFunction>;
 }
 
-export interface IStrategyRoot extends ISignable {
+export interface IStrategy {
   id: string;
+  address: string;
+  symbol: string;
+  name: string;
+  version: string;
   baseId: string;
-  decimals: number;
-  // strategyContract: Strategy;
-}
+  decimals: string;
 
-export interface IPoolRoot extends ISignable {}
-
-export interface ISeries extends ISeriesRoot {
-  apr: string;
-  baseReserves: BigNumber;
-  fyTokenReserves: BigNumber;
-  fyTokenRealReserves: BigNumber;
-  totalSupply: BigNumber;
-  totalSupply_: string;
-
-  poolTokens?: BigNumber | undefined;
-  poolTokens_?: string | undefined;
-  fyTokenBalance?: BigNumber | undefined;
-  fyTokenBalance_?: string | undefined;
-
-  poolPercent?: string | undefined;
-
-  seriesIsMature: boolean;
-}
-
-export interface IAsset extends IAssetRoot {
-  isYieldBase: boolean;
-  balance: BigNumber;
-  balance_: string;
-  hasLadleAuth: boolean;
-  hasJoinAuth: boolean;
-}
-
-export interface IStrategy extends IStrategyRoot {
   currentSeriesId: string;
   currentPoolAddr: string;
   nextSeriesId: string;
@@ -124,5 +73,3 @@ export interface IStrategy extends IStrategyRoot {
   accountPoolBalance_?: string;
   accountPoolPercent?: string | undefined;
 }
-
-export interface IPool extends IPoolRoot {}
