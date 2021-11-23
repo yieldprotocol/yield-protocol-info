dd6d852e6b4cd69456b11b6c4356c9e3804147ca
marcomariscal <marco.a.mariscal@gmail.com>
2021-11-23 15:53:52 -0500

    update types

--- src/state/actions/application.ts
+++ src/state/actions/application.ts
@@ -1,4 +1,4 @@
 import { ActionType } from '../actionTypes/application';
 
-export const toggleDarkMode = (darkMode: boolean) => ({ type: ActionType.TOGGLE_DARK_MODE, darkMode });
-export const updateVersion = (version: string) => ({ type: ActionType.VERSION, version });
+export const toggleDarkMode = (darkMode: boolean) => ({ type: ActionType.TOGGLE_DARK_MODE, payload: darkMode });
+export const updateVersion = (version: string) => ({ type: ActionType.VERSION, payload: version });
