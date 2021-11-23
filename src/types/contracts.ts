1dd56e03bd0df0801a9b0ee829d4bbb71ac1e3bb
marcomariscal <marco.a.mariscal@gmail.com>
2021-11-23 16:02:58 -0500

    update types

--- src/types/contracts.ts
+++ src/types/contracts.ts
@@ -1,4 +1,5 @@
 import { Contract } from 'ethers';
+import { ActionType } from '../state/actionTypes/contracts';
 
 export interface IContractState {
   eventsLoading: boolean;
@@ -6,22 +7,65 @@ export interface IContractState {
   events: {};
   roles: {};
   contractMap: IContractMap;
-  eventArgsPropsMap: IEventArgPropsMap;
+  eventArgsPropsMap: IEventArgsPropsMap;
+}
+
+export type IContractAction =
+  | IEventsLoadingAction
+  | IUpdateEventsAction
+  | IUpdateContractMapAction
+  | IEventArgsPropsMapAction
+  | IRolesLoadingAction
+  | IUpdateRolesAction
+  | IResetAction;
+
+export interface IEventsLoadingAction {
+  type: ActionType.EVENTS_LOADING;
+  payload: boolean;
+}
+
+export interface IUpdateEventsAction {
+  type: ActionType.UPDATE_EVENTS;
+  payload: any;
+}
+
+export interface IUpdateContractMapAction {
+  type: ActionType.UPDATE_CONTRACT_MAP;
+  payload: IContractMap;
+}
+
+export interface IEventArgsPropsMapAction {
+  type: ActionType.UPDATE_EVENT_ARGS_PROPS_MAP;
+  payload: IEventArgsPropsMap;
+}
+
+export interface IRolesLoadingAction {
+  type: ActionType.ROLES_LOADING;
+  payload: boolean;
+}
+
+export interface IUpdateRolesAction {
+  type: ActionType.UPDATE_ROLES;
+  payload: any;
+}
+
+export interface IResetAction {
+  type: ActionType.RESET;
 }
 
 export interface IContractMap {
   [name: string]: Contract;
 }
 
-export interface IEventArgPropsMap {
-  [name: string]: IEventArgProps;
+export interface IEventArgsPropsMap {
+  [name: string]: IEventArgsProps;
 }
 
-export interface IEventArgProps {
-  [name: string]: IEventArg[];
+export interface IEventArgsProps {
+  [name: string]: IEventArgs[];
 }
 
-interface IEventArg {
+interface IEventArgs {
   name: string;
   type: string;
 }
