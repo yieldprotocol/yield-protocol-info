fa43c1ab5b47e4fdfeb142e8bd215009ba53ea7e
marcomariscal <marco.a.mariscal@gmail.com>
2021-11-30 17:31:31 -0500

    refactor and remove unnecessary typing

--- src/state/hooks/useStrategyReturns.ts
+++ src/state/hooks/useStrategyReturns.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BigNumber, Contract, ethers } from 'ethers';
+import { ethers } from 'ethers';
 import { formatDistanceStrict } from 'date-fns';
 import { useBlockNum } from './useBlockNum';
 import { burnFromStrategy, SECONDS_PER_YEAR } from '../../utils/yieldMath';
@@ -13,53 +13,53 @@ import * as contracts from '../../contracts';
  * @param previousBlocks number of blocks to use for comparison (lookback window)
  */
 export const useStrategyReturns = (strategy: IStrategy, previousBlocks: number) => {
-  const provider: ethers.providers.JsonRpcProvider = useAppSelector((st) => st.chain.provider);
-  const currentBlock: number | null = useBlockNum();
+  const { provider } = useAppSelector((st) => st.chain);
+  const currentBlock = useBlockNum();
   const [previousBlock, setPreviousBlock] = useState<number>();
 
   const [strategyReturns, setStrategyReturns] = useState<string | null>(null);
   // number of seconds between comparison timeframe curr and pre
   const [secondsCompare, setSecondsCompare] = useState<number | null>(null);
   const [secondsToDays, setSecondsToDays] = useState<string | null>(null);
-  const [previousBlockTimestamp, setPreviousBlockTimestamp] = useState<any>();
-  const [currBlockTimestamp, setCurrBlockTimestamp] = useState<any>();
+  const [previousBlockTimestamp, setPreviousBlockTimestamp] = useState<number>();
+  const [currBlockTimestamp, setCurrBlockTimestamp] = useState<number>();
 
   useEffect(() => {
-    const _getStrategyBaseValuePerShare = async (blockNum: number) => {
+    const _getStrategyBaseValuePerShare = async (blockTag: number) => {
       try {
-        const poolContract: Contract = contracts.Pool__factory.connect(strategy.poolAddress, provider);
-        const strategyContract: Contract = contracts.Strategy__factory.connect(strategy.address, provider);
+        const poolContract = contracts.Pool__factory.connect(strategy.poolAddress, provider!);
+        const strategyContract = contracts.Strategy__factory.connect(strategy.address, provider!);
 
         const [[base, fyTokenVirtual], poolTotalSupply, strategyTotalSupply, decimals, fyTokenToBaseCostEstimate] =
           await Promise.all([
-            await poolContract.getCache({ blockTag: blockNum }),
-            await poolContract.totalSupply({ blockTag: blockNum }),
-            await strategyContract.totalSupply({ blockTag: blockNum }),
-            await poolContract.decimals({ blockTag: blockNum }),
+            await poolContract.getCache({ blockTag }),
+            await poolContract.totalSupply({ blockTag }),
+            await strategyContract.totalSupply({ blockTag }),
+            await poolContract.decimals({ blockTag }),
             await poolContract.sellFYTokenPreview(
-              BigNumber.from(1).mul(BigNumber.from(10).pow(await poolContract.decimals())),
+              ethers.utils.parseUnits('1', await poolContract.decimals({ blockTag })),
               {
-                blockTag: blockNum,
+                blockTag,
               }
             ), // estimate the base value of 1 fyToken unit
           ]);
 
         // the real balance of fyTokens in the pool
-        const fyTokenReal: BigNumber = (fyTokenVirtual as BigNumber).sub(poolTotalSupply as BigNumber);
+        const fyTokenReal = fyTokenVirtual.sub(poolTotalSupply);
 
         // the estimated base value of all fyToken in the pool
-        const fyTokenToBaseValueEstimate: BigNumber = fyTokenReal
+        const fyTokenToBaseValueEstimate = fyTokenReal
           .mul(fyTokenToBaseCostEstimate)
-          .div(BigNumber.from(1).mul(BigNumber.from(10).pow(decimals)));
+          .div(ethers.utils.parseUnits('1', decimals));
 
         // total estimated base value in pool
-        const totalBaseValue: BigNumber = base.add(fyTokenToBaseValueEstimate);
+        const totalBaseValue = base.add(fyTokenToBaseValueEstimate);
 
         // number of pool lp tokens associated with a strategy
-        const poolLpReceived: BigNumber = burnFromStrategy(poolTotalSupply, strategyTotalSupply, strategyTotalSupply);
+        const poolLpReceived = burnFromStrategy(poolTotalSupply, strategyTotalSupply, strategyTotalSupply);
 
         // the amount of base per strategy LP token
-        const baseValuePerStrategyLpToken: number =
+        const baseValuePerStrategyLpToken =
           (Number(totalBaseValue) / Number(poolTotalSupply)) * (Number(poolLpReceived) / Number(poolTotalSupply));
 
         return baseValuePerStrategyLpToken;
@@ -73,17 +73,17 @@ export const useStrategyReturns = (strategy: IStrategy, previousBlocks: number) 
     const _getStrategyReturns = async () => {
       if (strategy && currentBlock && previousBlock && currBlockTimestamp && previousBlockTimestamp) {
         try {
-          const baseValuePerShareCurr: number = await _getStrategyBaseValuePerShare(Number(currentBlock));
-          const baseValuePerSharePre: number = await _getStrategyBaseValuePerShare(Number(previousBlock));
+          const baseValuePerShareCurr = await _getStrategyBaseValuePerShare(currentBlock);
+          const baseValuePerSharePre = await _getStrategyBaseValuePerShare(previousBlock);
 
-          const returns: number = Number(baseValuePerShareCurr) / Number(baseValuePerSharePre) - 1;
+          const returns = baseValuePerShareCurr / baseValuePerSharePre - 1;
 
-          const secondsBetween: number = currBlockTimestamp - previousBlockTimestamp;
+          const secondsBetween = currBlockTimestamp - previousBlockTimestamp;
           setSecondsCompare(secondsBetween);
-          const periods: number = SECONDS_PER_YEAR / secondsBetween;
+          const periods = SECONDS_PER_YEAR / secondsBetween;
 
-          const apy: number = (1 + returns / periods) ** periods - 1;
-          const apy_: string = (apy * 100).toString();
+          const apy = (1 + returns / periods) ** periods - 1;
+          const apy_ = (apy * 100).toString();
           setStrategyReturns(apy_);
           return apy_;
         } catch (e) {
@@ -100,10 +100,10 @@ export const useStrategyReturns = (strategy: IStrategy, previousBlocks: number) 
     (async () => {
       if (currentBlock && provider) {
         try {
-          const _preBlock: number = Number(currentBlock) - previousBlocks; // use around 7 days ago timeframe in blocktime
+          const _preBlock = currentBlock - previousBlocks; // use around 7 days ago timeframe in blocktime
           setPreviousBlock(_preBlock);
-          setPreviousBlockTimestamp((await provider.getBlock(Number(_preBlock))).timestamp);
-          setCurrBlockTimestamp((await provider.getBlock(Number(currentBlock))).timestamp);
+          setPreviousBlockTimestamp((await provider.getBlock(_preBlock)).timestamp);
+          setCurrBlockTimestamp((await provider.getBlock(currentBlock)).timestamp);
         } catch (e) {
           console.log('could not get timestamps', e);
         }
@@ -113,7 +113,7 @@ export const useStrategyReturns = (strategy: IStrategy, previousBlocks: number) 
 
   /* translate the comparison seconds to days */
   useEffect(() => {
-    const _secondsToDays: string = formatDistanceStrict(
+    const _secondsToDays = formatDistanceStrict(
       new Date(1, 1, 0, 0, 0, 0),
       new Date(1, 1, 0, 0, 0, secondsCompare || 0),
       {
