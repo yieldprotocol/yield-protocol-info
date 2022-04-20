cd47b30e391bf14904367fbf2522c79fd9990133
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-20 13:29:01 -0400

    condense

--- src/pages/strategies/index.tsx
+++ src/pages/strategies/index.tsx
@@ -13,7 +13,7 @@ export const getServerSideProps = async ({ query }) => {
   const chainId = (query.chainId || 1) as number;
   const provider = new ethers.providers.JsonRpcProvider(process.env[`REACT_APP_RPC_URL_${chainId.toString()}`]);
   const strategyMap = await getStrategies(provider);
-  const strategiesList = [...Object.values(strategyMap)].sort((s1, s2) => (s1.name < s2.name ? -1 : 1));
+  const strategiesList = Object.values(strategyMap).sort((s1, s2) => (s1.name < s2.name ? -1 : 1));
 
   return { props: { strategiesList } };
 };
