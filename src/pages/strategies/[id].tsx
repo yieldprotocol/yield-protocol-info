f53ae0bef9627332dc374c057b29d7eddaf4f984
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-20 13:25:13 -0400

    refactor getting strategies

--- src/pages/strategies/[id].tsx
+++ src/pages/strategies/[id].tsx
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router';
 import Strategy from '../../components/views/Strategy';
+import useStrategies from '../../hooks/useStrategies';
 
-const StrategyPage = () => {
-  return <Strategy />;
+const AssetPage = () => {
+  const strategyMap = useStrategies();
+  const router = useRouter();
+  const { id: strategyId } = router.query;
+  if (!strategyMap || !strategyId) return null;
+
+  const strategy = strategyMap[strategyId as string];
+
+  return <Strategy strategy={strategy} />;
 };
 
-export default StrategyPage;
+export default AssetPage;
--- src/pages/strategies/index.tsx
+++ src/pages/strategies/index.tsx
@@ -1,7 +1,19 @@
+import { ethers } from 'ethers';
+import { InferGetServerSidePropsType } from 'next';
 import Strategies from '../../components/views/Strategies';
+import { getStrategies } from '../../lib/chain';
 
-const StrategiesPage = () => {
-  return <Strategies />;
-};
+const StrategiesPage = ({ strategiesList }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
+  <Strategies strategiesList={strategiesList} />
+);
 
 export default StrategiesPage;
+
+export const getServerSideProps = async ({ query }) => {
+  const chainId = (query.chainId || 1) as number;
+  const provider = new ethers.providers.JsonRpcProvider(process.env[`REACT_APP_RPC_URL_${chainId.toString()}`]);
+  const strategyMap = await getStrategies(provider);
+  const strategiesList = [...Object.values(strategyMap)].sort((s1, s2) => (s1.name < s2.name ? -1 : 1));
+
+  return { props: { strategiesList } };
+};
