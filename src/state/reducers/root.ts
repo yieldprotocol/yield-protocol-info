122623c1b8b8e4726d63c2c43d10bb0df18e8c56
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-21 17:52:39 -0400

    remove unnecessary

--- src/state/reducers/root.ts
+++ src/state/reducers/root.ts
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux';
 import chain from './chain';
 import contracts from './contracts';
 import vaults from './vaults';
-import application from './application';
 
 export default combineReducers({
-  application,
   chain,
   contracts,
   vaults,
