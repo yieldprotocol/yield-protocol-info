da49861732fa0cb9719442a0c6b27b5125a59488
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-21 17:29:30 -0400

    fix fetch series between chains

--- src/pages/series/index.tsx
+++ src/pages/series/index.tsx
@@ -1,17 +1,19 @@
 import { ethers } from 'ethers';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import SeriesList from '../../components/views/SeriesList';
+import useSeries from '../../hooks/useSeries';
 import { getSeries } from '../../lib/chain';
 import { getContracts } from '../../lib/contracts';
 
-const SeriesPage = ({ seriesList }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
-  <SeriesList seriesList={seriesList} />
-);
+const SeriesPage = ({ seriesList }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const seriesMap = useSeries();
+  return <SeriesList seriesList={seriesMap ? Object.values(seriesMap) : seriesList} />;
+};
 
 export default SeriesPage;
 
-export const getServerSideProps = async ({ query }) => {
-  const chainId = (query.chainId || 1) as number;
+export const getStaticProps = async () => {
+  const chainId = 1;
   const provider = new ethers.providers.JsonRpcProvider(process.env[`REACT_APP_RPC_URL_${chainId.toString()}`]);
   const contractMap = await getContracts(provider, chainId);
   const seriesMap = await getSeries(provider, contractMap);
