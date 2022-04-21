cac2931d8f9b049d658c30bd8992e7f79f6159a8
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-21 15:37:26 -0400

    remove unused

--- src/state/hooks/useTvl.ts
+++ src/state/hooks/useTvl.ts
@@ -1,17 +1,3 @@
-import { useEffect } from 'react';
-import { getAssetsTvl } from '../actions/chain';
-import { useAppDispatch, useAppSelector } from './general';
-
-const useTvl = () => {
-  const dispatch = useAppDispatch();
-  const { provider, chainId, assets, series } = useAppSelector(({ chain }) => chain);
-  const { contractMap } = useAppSelector(({ contracts }) => contracts);
-
-  useEffect(() => {
-    if (assets && contractMap && series && provider && chainId) {
-      dispatch(getAssetsTvl(assets, contractMap, series, provider, chainId));
-    }
-  }, [assets, contractMap, dispatch, provider, series, chainId]);
-};
+const useTvl = () => {};
 
 export default useTvl;
