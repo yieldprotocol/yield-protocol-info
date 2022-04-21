247f2faa12379d0d44413c102b5cefa8012a4cca
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-21 16:28:05 -0400

    dynamic layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,16 @@
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
-import Layout from '../components/Layout';
-import { wrapper } from '../state/stores/store';
 import '../styles/globals.css';
 
 const DynamicWeb3Provider = dynamic(() => import('../hooks/useWeb3Provider'), { ssr: false });
+const DynamicLayout = dynamic(() => import('../components/Layout'), { ssr: false });
 
 const App = ({ Component, pageProps }: AppProps) => (
   <DynamicWeb3Provider>
-    <Layout>
+    <DynamicLayout>
       <Component {...pageProps} />
-    </Layout>
+    </DynamicLayout>
   </DynamicWeb3Provider>
 );
 
-export default wrapper.withRedux(App);
+export default App;
