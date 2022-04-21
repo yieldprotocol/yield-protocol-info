261dfc1f82e9394e799164730357198780e534d3
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-21 16:28:18 -0400

    use getStaticProps

--- src/pages/assets/index.tsx
+++ src/pages/assets/index.tsx
@@ -1,17 +1,15 @@
 import { ethers } from 'ethers';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import Assets from '../../components/views/Assets';
 import { getAssets } from '../../lib/chain';
 import { getContracts } from '../../lib/contracts';
 
-const AssetsPage = ({ assetList }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
-  <Assets assetList={assetList} />
-);
+const AssetsPage = ({ assetList }: InferGetStaticPropsType<typeof getStaticProps>) => <Assets assetList={assetList} />;
 
 export default AssetsPage;
 
-export const getServerSideProps = async ({ query }) => {
-  const chainId = (query.chainId || 1) as number;
+export const getStaticProps = async () => {
+  const chainId = 1;
   const provider = new ethers.providers.JsonRpcProvider(process.env[`REACT_APP_RPC_URL_${chainId.toString()}`]);
   const contractMap = await getContracts(provider, chainId);
   const assetMap = await getAssets(provider, contractMap);
