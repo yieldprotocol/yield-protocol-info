6298e39968b4dba320645aa326beef291e6e0b50
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-22 08:00:23 -0400

    sort

--- src/pages/series/index.tsx
+++ src/pages/series/index.tsx
@@ -4,10 +4,16 @@ import SeriesList from '../../components/views/SeriesList';
 import useSeries from '../../hooks/useSeries';
 import { getSeries } from '../../lib/chain';
 import { getContracts } from '../../lib/contracts';
+import { ISeriesMap } from '../../types/chain';
+
+const handleSort = (strategyMap: ISeriesMap) =>
+  Object.values(strategyMap)
+    .sort((s1, s2) => (s1.name < s2.name ? -1 : 1))
+    .sort((s1, s2) => s1.maturity - s2.maturity);
 
 const SeriesPage = ({ seriesList }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const seriesMap = useSeries();
-  return <SeriesList seriesList={seriesMap ? Object.values(seriesMap) : seriesList} />;
+  return <SeriesList seriesList={seriesMap ? handleSort(seriesMap) : seriesList} />;
 };
 
 export default SeriesPage;
@@ -18,9 +24,7 @@ export const getStaticProps = async () => {
   const contractMap = await getContracts(provider, chainId);
   const seriesMap = await getSeries(provider, contractMap);
 
-  const seriesList = Object.values(seriesMap)
-    .sort((s1, s2) => (s1.name < s2.name ? -1 : 1))
-    .sort((s1, s2) => s1.maturity - s2.maturity);
+  const seriesList = handleSort(seriesMap);
 
   return { props: { seriesList } };
 };
