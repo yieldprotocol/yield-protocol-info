2372dc4e507ac30c4811bfe23ab1cdb42d2c7242
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-24 08:25:25 -0400

    update chain id in redux on mount

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useLocalStorage } from '../hooks/useLocalStorage';
+import { updateChainId } from '../state/actions/application';
+import { useAppDispatch } from '../state/hooks/general';
 import { wrapper } from '../state/stores/store';
 import '../styles/globals.css';
 import { CHAIN_ID_LOCAL_STORAGE } from '../utils/constants';
@@ -11,6 +13,7 @@ const DynamicWeb3Provider = dynamic(() => import('../hooks/useWeb3Provider'), { 
 const DynamicLayout = dynamic(() => import('../components/Layout'), { ssr: false });
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const dispatch = useAppDispatch();
   const router = useRouter();
   const [chainId] = useLocalStorage(CHAIN_ID_LOCAL_STORAGE, '1');
 
@@ -19,6 +22,10 @@ const App = ({ Component, pageProps }: AppProps) => {
     const url = { pathname: router.asPath.split('?')[0], query: { chainId } };
     if (chainId !== router.query.chainId) router.replace(url);
   }, [chainId]);
+
+  useEffect(() => {
+    dispatch(updateChainId(+chainId)); // update redux on mount
+  }, []);
 
   return (
     <DynamicWeb3Provider>
