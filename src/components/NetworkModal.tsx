00a711c87a1733d3f8b37e7f8a9d1598f3accaf5
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-24 07:56:07 -0400

    update chain id in redux and use chain id from localstorage

--- src/components/NetworkModal.tsx
+++ src/components/NetworkModal.tsx
@@ -1,33 +1,30 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { useWeb3React } from '@web3-react/core';
-import Router, { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { CHAIN_INFO, SUPPORTED_CHAIN_IDS } from '../config/chainData';
 import { useLocalStorage } from '../hooks/useLocalStorage';
+import { updateChainId } from '../state/actions/application';
+import { useAppDispatch, useAppSelector } from '../state/hooks/general';
 import { CHAIN_ID_LOCAL_STORAGE } from '../utils/constants';
 
 const NetworkModal = () => {
   const router = useRouter();
-  const [cachedChainId, setCachedChainId] = useLocalStorage(CHAIN_ID_LOCAL_STORAGE, '1');
-  const { chainId, connector } = useWeb3React();
+  const dispatch = useAppDispatch();
+  const { chainId } = useAppSelector(({ application }) => application);
+  const [cachedChainId, setCachedChainId] = useLocalStorage(CHAIN_ID_LOCAL_STORAGE, chainId.toString());
   const [showModal, setShowModal] = useState<boolean>(false);
-
-  // update the connection to the proper chainId
-  useEffect(() => {
-    connector.activate(+cachedChainId);
-  }, [cachedChainId, connector]);
 
   return (
     <>
       <div>
-        {chainId && (
+        {cachedChainId && (
           <button
             type="button"
             onClick={() => setShowModal(!showModal)}
             className="text-gray-900 hover:bg-green-500 bg-green-300  flex-shrink-0 inline-flex items-center justify-center overflow-hidden font-medium truncate focus:outline-none focus-visible:ring focus-visible:ring-offset-2 focus-visible:ring-green-800 focus-visible:ring-offset-green-900 transition dark:text-gray-900 dark:hover:bg-green-500  text-md leading-5 rounded-lg px-3 py-2"
           >
-            {CHAIN_INFO.get(chainId)?.name}
+            {CHAIN_INFO.get(+cachedChainId)?.name}
           </button>
         )}
       </div>
@@ -43,12 +40,13 @@ const NetworkModal = () => {
                   <div className="text-lg leading-6 font-medium dark:text-white">Choose Network</div>
                   <div className="relative p-6">
                     <div className="my-4 text-gray-900 text-lg leading-relaxed">
-                      {SUPPORTED_CHAIN_IDS.map((id: number) => (
+                      {SUPPORTED_CHAIN_IDS.map((id) => (
                         <button
                           key={id}
                           className="my-2 w-full text-gray-900 hover:bg-green-500 bg-green-300 items-center justify-center overflow-hidden font-medium focus:outline-none focus-visible:ring focus-visible:ring-offset-2 focus-visible:ring-green-800 focus-visible:ring-offset-green-900 transition dark:hover:bg-green-500 text-md rounded-lg py-2"
                           onClick={() => {
-                            setCachedChainId(id.toString());
+                            setCachedChainId(id.toString()); // set chain id in local storage
+                            dispatch(updateChainId(id)); // set chain id in redux
                             const url = { pathname: router.asPath.split('?')[0], query: { chainId: id } };
                             // const urlAs = { pathname: router.asPath };
                             // router.push(url, urlAs);
