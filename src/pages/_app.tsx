80f2e96b78f85ce3371ccb787a386c67ddf02ed0
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-24 07:56:27 -0400

    make sure chain id query param always matches localstorage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,32 @@
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 import { wrapper } from '../state/stores/store';
 import '../styles/globals.css';
+import { CHAIN_ID_LOCAL_STORAGE } from '../utils/constants';
 
 const DynamicWeb3Provider = dynamic(() => import('../hooks/useWeb3Provider'), { ssr: false });
 const DynamicLayout = dynamic(() => import('../components/Layout'), { ssr: false });
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <DynamicWeb3Provider>
-    <DynamicLayout>
-      <Component {...pageProps} />
-    </DynamicLayout>
-  </DynamicWeb3Provider>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  const [chainId] = useLocalStorage(CHAIN_ID_LOCAL_STORAGE, '1');
+
+  // update the url if the cached chain id doesn't match the query params
+  useEffect(() => {
+    const url = { pathname: router.asPath.split('?')[0], query: { chainId } };
+    if (chainId !== router.query.chainId) router.replace(url);
+  }, [chainId]);
+
+  return (
+    <DynamicWeb3Provider>
+      <DynamicLayout>
+        <Component {...pageProps} />
+      </DynamicLayout>
+    </DynamicWeb3Provider>
+  );
+};
 
 export default wrapper.withRedux(App);
