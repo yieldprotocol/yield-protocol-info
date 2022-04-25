348adff9e61872f55ca17c3f9aaaac54dfe2bd34
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-25 11:49:26 -0400

    remoev unnecessary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import { updateChainId } from '../state/actions/application';
@@ -14,14 +13,7 @@ const DynamicLayout = dynamic(() => import('../components/Layout'), { ssr: false
 
 const App = ({ Component, pageProps }: AppProps) => {
   const dispatch = useAppDispatch();
-  const router = useRouter();
   const [chainId] = useLocalStorage(CHAIN_ID_LOCAL_STORAGE, '1');
-
-  // update the url if the cached chain id doesn't match the query params
-  useEffect(() => {
-    const url = { pathname: router.asPath.split('?')[0], query: { chainId } };
-    if (chainId !== router.query.chainId) router.replace(url);
-  }, [chainId]);
 
   useEffect(() => {
     dispatch(updateChainId(+chainId)); // update redux on mount
